8b4a6aabfda62638b1378a00d79b622f17434642
sooshiance <s.katafract@gmail.com>
2024-01-27 16:30:36 +0400

    add some features for login and products

--- local/locals.py
+++ local/locals.py
@@ -1,5 +1,5 @@
 from flask import *
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin, login_user
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -13,12 +13,17 @@ db = SQLAlchemy(app)
 
 
 class Product(db.Model):
-      id = db.Column(db.String(30), db.Integer, primary_key=True, serializable=False)
+      id    = db.Column('product_id', db.Integer, primary_key=True, serializable=False)
       title = db.Column(db.String(50), nullable=False)
+      price = db.Column(db.String(21), nullable=False)
+      
+      def __init__(self, title, price) -> None:
+           self.title = title
+           self.price = price
 
 
-class User(db.Model):
-      id = db.Column('user_id', db.Integer, primary_key = True)
+class User(UserMixin, db.Model):
+      id       = db.Column('user_id', db.Integer, primary_key = True, serializable=False)
       username = db.Column(db.String(30), nullable=False, unique=True)
       password = db.Column(db.String(50), nullable=False)
 
@@ -37,25 +42,42 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.sqlite3'
 app.config["FLASK_SECRET_KEY"] = "ENTER YOUR SECRET KEY"
 
 
+############################### User login ###############################
+
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return User.query.get(user_id)
+
+
 ############################### handle requests ###############################
 
 
 @app.route('/login',methods = ['POST'])
 def login():
       if request.method == "POST":
-            username=request.data.get('username')
-            password=request.data.get('password')
-            return 
+        user = User.query.filter_by(username=request.data.get("username")).first()
+        if user.password == request.data.get("password"):
+            if login_user(user):
+                  return jsonify(user)
+            else:
+                  return jsonify("user has no password")
+            
 
 
 @app.route("product/<integer:id>/", methods=['GET'])
 def getProduct(id, user):
-      return
+      return jsonify()
 
 
 @app.route("", methods=["POST"])
-def unnamed():
-      return 
+def searchProduct():
+      if request.method == "":
+            return jsonify()
 
 
 ############################### app run ###############################
